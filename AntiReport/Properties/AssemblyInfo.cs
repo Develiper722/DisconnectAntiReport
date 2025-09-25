dbefbf64c19eff7d178b2a1a67a7f2aab67ee3e6
Develiper722 <marcusvrwastaken@gmail.com>
2025-09-25 16:44:39 +0100

    Create AssemblyInfo.cs

---
+++ AntiReport/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("AntiReport")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AntiReport")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("78358a84-663d-4889-aa0d-4000f48ddf29")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
